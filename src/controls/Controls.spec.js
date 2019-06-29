940f2f5e015777d321410c5326e36978bb45ee0f
LD <LDinho@users.noreply.github.com>
2019-06-29 05:25:57 -0400

    add lock gate test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { render, fireEvent, cleanup } from 'react-testing-library';
-afterEach(cleanup);
+
+afterEach(cleanup); // cleans up tests so tests starts with clean slate
+
+import "jest-dom/extend-expect";
 
 import Controls from './Controls';
 
@@ -23,5 +26,23 @@ describe('Control Component', () => {
     expect(mock).toHaveBeenCalled();
   });
 
+  it("lock gate should be disabled by default", () => {
+    const { getByText } = render(<Controls />);
+    const lockButton = getByText(/lock gate/i);
+    expect(lockButton).toBeDisabled();
+  });
+
+  it('lock gate function should work', () => {
+    const mock = jest.fn();
+
+    const { getByText, queryByText } = render(<Controls toggleClosed={mock}/>);
+
+    const button = getByText("Close Gate");
+
+    fireEvent.click(button);
+
+    const lockGateButton = queryByText("Lock Gate");
+    expect(lockGateButton.textContent).toEqual("Lock Gate");
+  })
 
 });
