b8d699a491f06471f66a5bca802bcdc2d1a9b068
LD <LDinho@users.noreply.github.com>
2019-06-29 05:33:47 -0400

    add open gate disabled test
    -open gate is disabled when gate is locked

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import "jest-dom/extend-expect";
+afterEach(cleanup);
+
 import Dashboard from './Dashboard';
 
 describe('Dashboard Component', () => {
@@ -7,6 +11,23 @@ describe('Dashboard Component', () => {
     const domTree = renderer.create(<Dashboard />);
 
     expect(domTree.toJSON()).toMatchSnapshot();
+  });
+
+  it("open gate should be disabled when gate is locked", () => {
+    const mock = jest.fn();
+
+    const { getByText, queryByText } = render(<Dashboard toggleClosed={mock} toggleLocked={mock} />);
+
+    const closeGateButton = getByText("Close Gate");
+
+    fireEvent.click(closeGateButton);
+
+    const lockGateButton = queryByText("Lock Gate");
+    fireEvent.click(lockGateButton);
+
+    const openGateButton = getByText("Open Gate")
+    expect(openGateButton).toBeDisabled();
+
   });
 
 });
