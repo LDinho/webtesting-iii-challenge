746a698b426c4523acdc7488c86eb00d94ec477c
LD <LDinho@users.noreply.github.com>
2019-06-29 04:51:11 -0400

    add Control & Display component snapshot tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Controls from './Controls';
+
+describe('Control Component', () => {
+  it('matches snapshot', () => {
+    const domTree = renderer.create(<Controls />);
+
+    expect(domTree.toJSON()).toMatchSnapshot();
+  });
+
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Display from './Display';
+
+describe('Display Component', () => {
+  it('matches snapshot', () => {
+    const domTree = renderer.create(<Display />);
+
+    expect(domTree.toJSON()).toMatchSnapshot();
+  });
+
+});
