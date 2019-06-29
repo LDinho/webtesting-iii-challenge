81635f32affaf2cb215b6de5ad71ecb36b0d9b8b
LD <LDinho@users.noreply.github.com>
2019-06-29 05:39:31 -0400

    add unlock gate enabled test
    -unlock gate is enabled when gate is locked

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -30,4 +30,21 @@ describe('Dashboard Component', () => {
 
   });
 
+  it("unlock gate enabled when gate is locked", () => {
+    const mock = jest.fn();
+
+    const { getByText, queryByText } = render(<Dashboard toggleClosed={mock} toggleLocked={mock} />);
+
+    const closeGateButton = getByText("Close Gate");
+
+    fireEvent.click(closeGateButton);
+
+    const lockGateButton = queryByText("Lock Gate");
+    fireEvent.click(lockGateButton);
+
+    const unlockGateButton = getByText("Unlock Gate")
+    expect(unlockGateButton.textContent).toEqual("Unlock Gate");;
+
+  });
+
 });
